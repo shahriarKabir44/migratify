1f8fba2d0950e8f7f049c494036b9a3e45f56790
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-09-21 01:14:53 +0600

    corrected path

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -5,7 +5,7 @@ class DBConnection {
     static connection = null
 
     static async initConnection() {
-        const env = require(process.cwd() + '/config.json')
+        const env = require(process.cwd() + '/migrations/config.json')
 
         return new Promise((resolve, reject) => {
             this.connection = mysql.createConnection({
