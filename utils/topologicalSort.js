1b5df1ed7c98ef5dd15264a1f5eff7ac45aa87ea
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-09-21 15:33:53 +0600

    updated topologicl sort

--- utils/topologicalSort.js
+++ utils/topologicalSort.js
@@ -9,17 +9,18 @@ function topologicalSort(vertices, edges) {
     for (let vertex of vertices) {
         nodeIndices[vertex] = index++
     }
-    edges = structuredClone(edges)
+    let connections = []
     for (let edge of edges) {
-        edge['source_table'] = nodeIndices[edge['source_table']]
-        edge['target_table'] = nodeIndices[edge['target_table']]
+        let a = nodeIndices[edge['source_table']]
+        let b = nodeIndices[edge['target_table']]
+        connections.push([a, b])
     }
     let outDegreeCount = new Array(vertices.length).fill(0)
     let indegreeNodes = []
     for (let n = 0; n < vertices.length; n++)indegreeNodes[n] = []
-    for (let edge of edges) {
-        outDegreeCount[edge['source_table']]++
-        indegreeNodes[edge['target_table']].push(edge['source_table'])
+    for (let edge of connections) {
+        outDegreeCount[edge[0]]++
+        indegreeNodes[edge[1]].push(edge[0])
     }
     let queue = []
     let res = []
@@ -29,13 +30,40 @@ function topologicalSort(vertices, edges) {
             break
         }
     }
+    let taken = new Set()
     while (queue.length) {
         let top = queue.shift()
         res.push(top)
+        taken.add(top)
+        if (indegreeNodes[top].length == 0) {
+            for (let node = 0; node < vertices.length; node++) {
+                if (outDegreeCount[node] == 0 && !taken.has(node)) {
+                    queue.push(node)
+                    taken.add(node)
+                    break
+                }
+            }
+            continue;
+        }
+        let found = 0
         for (let adj of indegreeNodes[top]) {
             outDegreeCount[adj]--
-            if (outDegreeCount[adj] == 0)
+            if (outDegreeCount[adj] == 0) {
                 queue.push(adj * 1)
+                taken.add(adj)
+
+                found = 1
+            }
+        }
+        if (found == 0) {
+            for (let node = 0; node < vertices.length; node++) {
+                if (outDegreeCount[node] == 0 && !taken.has(node)) {
+                    queue.push(node)
+                    taken.add(node)
+                    break
+                }
+            }
+            continue;
         }
     }
     return res.map(node => vertices[node])
